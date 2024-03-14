36f4571e23d15e9b0a19b207a472fe21344b101c
Oleg1front <151941470+Oleg1front@users.noreply.github.com>
2024-03-14 18:30:41 +0000

    Deploying to gh-pages from @ Oleg1front/goit-js-hw-11@c0f3e206c1cdfd699f99833a57406e145e4d4138 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,24 @@
-import"./assets/vendor-86291ea8.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&r(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();function i(){const c="42869495-3eaffa1d7f59c13a6a9af4ac7",o=a.value,n=`https://pixabay.com/api/?key=${c}&q=${o}&orientation=horizontal&safesearch=true`;fetch(n).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{console.log(r)}).catch(r=>{console.log(r)})}const u=document.querySelector(".form"),a=document.querySelector(".search-input");document.querySelector(".start-btn");u.addEventListener("submit",c=>{c.preventDefault(),i()});
+import"./assets/vendor-86291ea8.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&t(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function t(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();function c(l){const r="42869495-3eaffa1d7f59c13a6a9af4ac7",i=l.value,t=`https://pixabay.com/api/?key=${r}&q=${i}&orientation=horizontal&safesearch=true`;return fetch(t).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}function n(l){const r=document.querySelector(".gallery"),i=l.map(t=>`<li class="gallery-list-item">
+        <a class="gallery-link" href="${t.largeImageURL}">
+            <img src="${t.webformatURL}" alt="${t.tags}" class="gallery-list-item-img"        
+        </a>
+            <div class="text-wrapper">
+                <div class="item-text-wrapper">
+                    <p class ="gallery-list-item-text">Likes</p>
+                    <p class ="gallery-list-item-text">${t.likes}</p>
+                </div>
+                <div class="item-text-wrapper">
+                    <p class ="gallery-list-item-text">Views</p>
+                    <p class ="gallery-list-item-text">${t.views}</p>
+                </div>
+                <div class="item-text-wrapper">
+                    <p class ="gallery-list-item-text">Comments</p>
+                    <p class ="gallery-list-item-text">${t.comments}</p>
+                </div>
+                <div class="item-text-wrapper">
+                    <p class ="gallery-list-item-text">Downloads </p>
+                    <p class ="gallery-list-item-text">${t.downloads}</p>
+                </div>
+            </div>
+      </li>`).join("");r.insertAdjacentHTML("beforeend",i)}const o=document.querySelector(".form"),p=document.querySelector(".search-input");document.querySelector(".start-btn");o.addEventListener("submit",l=>{l.preventDefault(),c(p).then(({hits:r})=>{n(r)}).catch(r=>console.log(r)),o.reset()});
 //# sourceMappingURL=commonHelpers.js.map

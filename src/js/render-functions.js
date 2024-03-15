11bc848c1c3e23a81941127a7b46d4732fc9dd88
Oleh Liubimov <mtbs1996@gmail.com>
2024-03-15 14:22:34 +0200

    css, lightbox dont work

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,23 +4,23 @@ function createGalleryMarkup(items) {
     .map(item => {
       return `<li class="gallery-list-item">
         <a class="gallery-link" href="${item.largeImageURL}">
-            <img src="${item.webformatURL}" alt="${item.tags}" class="gallery-list-item-img"        
+            <img src="${item.webformatURL}" alt="${item.tags}" class="gallery-list-item-img"/>        
         </a>
             <div class="text-wrapper">
                 <div class="item-text-wrapper">
-                    <p class ="gallery-list-item-text">Likes</p>
+                    <p class ="gallery-list-item-text-1">Likes</p>
                     <p class ="gallery-list-item-text">${item.likes}</p>
                 </div>
                 <div class="item-text-wrapper">
-                    <p class ="gallery-list-item-text">Views</p>
+                    <p class ="gallery-list-item-text-1">Views</p>
                     <p class ="gallery-list-item-text">${item.views}</p>
                 </div>
                 <div class="item-text-wrapper">
-                    <p class ="gallery-list-item-text">Comments</p>
+                    <p class ="gallery-list-item-text-1">Comments</p>
                     <p class ="gallery-list-item-text">${item.comments}</p>
                 </div>
                 <div class="item-text-wrapper">
-                    <p class ="gallery-list-item-text">Downloads </p>
+                    <p class ="gallery-list-item-text-1">Downloads </p>
                     <p class ="gallery-list-item-text">${item.downloads}</p>
                 </div>
             </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('.form');
 export const searchInput = document.querySelector('.search-input');
 export const startBtn = document.querySelector('.start-btn');
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import fetchImg from './js/pixabay-api';
 import createGalleryMarkup from './js/render-functions';
@@ -15,3 +17,8 @@ form.addEventListener('submit', evt => {
 
   form.reset();
 });
+
+const lightbox = new simpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+lightbox.elements
